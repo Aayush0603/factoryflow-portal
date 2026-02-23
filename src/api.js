05935f1e61b36a0391c29b0009511f126749852f
Aayush <aayushdoshi126@gmail.com>
2026-02-23 16:39:19 +0530

    Connected to live backend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/public",
+  baseURL: "https://factoryflow-backend-a0vc.onrender.com/api/public",
 });
 
 export default API;
