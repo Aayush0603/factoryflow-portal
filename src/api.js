f620f934cd43a4ff2671b8e2d0ac01bb71d33351
Aayush <aayushdoshi126@gmail.com>
2026-02-25 17:19:47 +0530

    Updated api.js file

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://factoryflow-backend-a0vc.onrender.com/api/public",
+  baseURL: "https://factoryflow-backend-a0vc.onrender.com/api",
 });
 
 export default API;
