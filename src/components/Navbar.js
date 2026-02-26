4694a1e5ba3940774a5f988c5ace4df53adffc01
Aayush <aayushdoshi126@gmail.com>
2026-02-26 12:51:17 +0530

    Updated Navbar Feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,77 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "../index.css";
 
 function Navbar() {
   const navigate = useNavigate();
 
+  const token = localStorage.getItem("customerToken");
+  const user = JSON.parse(localStorage.getItem("customerUser") || "{}");
+
+  const handleLogout = () => {
+    localStorage.removeItem("customerToken");
+    localStorage.removeItem("customerUser");
+    navigate("/");
+  };
+
   return (
-    <div className="navbar">
-      <div className="nav-container">
-        <div className="nav-logo" onClick={() => navigate("/")}>
-          FactoryFlow
-        </div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "15px 30px",
+        background: "#111827",
+        color: "white"
+      }}
+    >
+      {/* Left Side */}
+      <div>
+        <Link to="/" style={linkStyle}>FactoryFlow</Link>
+        <Link to="/products" style={linkStyle}>Products</Link>
+        <Link to="/track-inquiry" style={linkStyle}>Track Inquiry</Link>
+      </div>
 
-        <div className="nav-links">
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
+      {/* Right Side */}
+      <div>
+        {!token ? (
+          <>
+            <Link to="/login" style={linkStyle}>Login</Link>
+            <Link to="/signup" style={linkStyle}>Signup</Link>
+          </>
+        ) : (
+          <>
+            <span style={{ marginRight: "10px" }}>
+              Hi, {user?.name}
+            </span>
 
-          <Link to="/products" className="nav-link">
-            Products
-          </Link>
+            <Link to="/account" style={linkStyle}>
+              My Account
+            </Link>
 
-          <Link to="/dealer-login" className="nav-link">
-            Dealer Login
-          </Link>
-
-          <Link to="/track-inquiry" className="nav-link">
-            Track Inquiry
-          </Link>
-        </div>
+            <button
+              onClick={handleLogout}
+              style={{
+                marginLeft: "10px",
+                padding: "5px 10px",
+                background: "#ef4444",
+                border: "none",
+                color: "white",
+                cursor: "pointer",
+                borderRadius: "4px"
+              }}
+            >
+              Logout
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
 }
+
+const linkStyle = {
+  marginRight: "15px",
+  color: "white",
+  textDecoration: "none"
+};
 
 export default Navbar;
